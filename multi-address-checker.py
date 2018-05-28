b82b61cb605f297fa396ad39c6f007b89c5c4d37
root <root@DESKTOP-MFS9J1J.localdomain>
2018-05-28 14:33:14 +0200

    Continue with script and addresses test file

--- multi-address-checker.py
+++ multi-address-checker.py
@@ -1,29 +1,47 @@
 #!/user/bin/python
 import fileinput
 import re
+import pybitcoin
 from pybitcoin import BitcoinPrivateKey #pip install pybitcoin
+from pybitcoin import BitcoinPublicKey #pip install pybitcoin
+import requests #pip install -U requests[socks]
+import re
 
 def main():
-	adresses=[]
+	addresses=[]
 	private_keys=[]
+	addresses_with_balance=[]
+	private_keys_with_balance=[]
 
 	#Find addresses and private keys inside STDIN
+	print("Searching for bitcoin addresses...")
+	print("")
 	for line in fileinput.input():
 
 		#Find all bitcoin addresses
-		print("Searching for Bitcoin addresses...")
-		for match in re.findall('^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$',line):
+		for match in re.findall('[13][a-km-zA-HJ-NP-Z1-9]{25,34}',line):
+			print("Match: " + match)
 			addresses.append(match)
 
 		#Find all bitcoin private keys
-		print("Searching for Bitcoin private keys...")
-		for match in re.findall('^[5KL][1-9A-HJ-NP-Za-km-z]{50,51}$',line):
-			private keys.append(match)
+		for match in re.findall('[5KL][1-9A-HJ-NP-Za-km-z]{50,51}',line):
+			print("Match: " + match)
+			private_keys.append(match)
+	print("")
 
+	print("Removing duplicate addresses (private key match)...")
+	print("")
 	#Remove duplicate public addresses (that correspond to some private key)
+	for private_key in private_keys:
+		address=BitcoinPrivateKey(private_key).public_key().address()
+		if address in addresses:
+			print("Removed: " + address)
+			addresses.remove(address)
+	print("")
 	
 
-
+def checkBalance(address):
+	#Check if private key, convert to public key
 
 
 
